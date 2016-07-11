a3e21962bb86c35b3a4be12956d385594e8e2f28
gevgev <gevgev@yahoo.com>
2016-07-11 12:00:44 -0700

    initial commit, reads/downloads gzip files from aws complete

---
+++ viewership-aggregator.go
@@ -0,0 +1,544 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func formatDefaultDate() string {
+	year, month, day := time.Now().Date()
+
+	return fmt.Sprintf("%4d%02d%02d", year, int(month), day)
+}
+
+func formatDate(date string) string {
+	return strings.Replace(strings.Replace(date, "/", "", -1), "-", "", -1)
+}
+
+const (
+	version     = "0.1"
+	MAXATTEMPTS = 3
+)
+
+var (
+	regionName      string
+	bucketName      string
+	dateFrom        string
+	dateTo          string
+	msoListFilename string
+	maxAttempts     int
+	concurrency     int
+
+	verbose bool
+	appName string
+
+	failedFilesChan         chan string
+	downloadedReportChannel chan bool
+
+	MSOLookup map[string]string
+	msoList   []MsoType
+)
+
+func init() {
+
+	flagRegion := flag.String("r", "us-west-2", "`AWS Region`")
+	flagBucket := flag.String("b", "daap-viewership-reports", "`Bucket name`")
+	flagDateFrom := flag.String("from", formatDefaultDate(), "`Date from`")
+	flagDateTo := flag.String("to", formatDefaultDate(), "`Date to`")
+	flagMsoFileName := flag.String("m", "mso-list.csv", "Filename for `MSO` list")
+	flagMaxAttempts := flag.Int("M", MAXATTEMPTS, "`Max attempts` to retry download from aws.s3")
+	flagConcurrency := flag.Int("c", 10, "The number of files to process `concurrent`ly")
+	flagHelp := flag.Bool("h", false, "Help")
+
+	flagVerbose := flag.Bool("v", true, "`Verbose`: outputs to the screen")
+
+	flag.Parse()
+	if flag.Parsed() {
+		appName = os.Args[0]
+
+		if *flagHelp {
+			usage()
+		}
+
+		regionName = *flagRegion
+		bucketName = *flagBucket
+		dateFrom = formatDate(*flagDateFrom)
+		dateTo = formatDate(*flagDateTo)
+		msoListFilename = *flagMsoFileName
+		maxAttempts = *flagMaxAttempts
+		concurrency = *flagConcurrency
+
+		verbose = *flagVerbose
+	} else {
+		usage()
+	}
+
+}
+
+func usage() {
+	fmt.Printf("%s, ver. %s\n", appName, version)
+	fmt.Println("Command line:")
+	fmt.Printf("\tprompt$>%s -r <aws_region> -b <s3_bucket_name> --from <date> --to <date> -m <mso-list-file-name> -M <max_retry>\n", appName)
+	flag.Usage()
+	os.Exit(-1)
+}
+
+func PrintParams() {
+	log.Printf("Provided: -r: %s, -b: %s, --from: %v, --to: %v, -m %s, -M %d, -v: %v\n",
+		regionName,
+		bucketName,
+		dateFrom,
+		dateTo,
+		msoListFilename,
+		maxAttempts,
+		verbose,
+	)
+
+}
+
+type MsoType struct {
+	Code string
+	Name string
+}
+
+// Read the list of MSO's and initialize the lookup map and array
+func getMsoNamesList() ([]MsoType, map[string]string) {
+	msoList := []MsoType{}
+	msoLookup := make(map[string]string)
+
+	msoFile, err := os.Open(msoListFilename)
+	if err != nil {
+		log.Fatalf("Could not open Mso List file: %s, Error: %s\n", msoListFilename, err)
+	}
+
+	r := csv.NewReader(msoFile)
+	r.TrimLeadingSpace = true
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatalf("Could not read MSO file: %s, Error: %s\n", msoListFilename, err)
+	}
+
+	for _, record := range records {
+		msoList = append(msoList, MsoType{record[0], record[1]})
+		msoLookup[record[0]] = record[1]
+	}
+	return msoList, msoLookup
+}
+
+// path per mso
+func formatPrefix(path, msoCode string) string {
+	return fmt.Sprintf("%s/%s/delta/", path, msoCode)
+}
+
+// converts/breaks the "20160601" string into yy, mm, dd
+func convertToDateParts(dtStr string) (yy, mm, dd int) {
+	yy, mm, dd = 0, 0, 0
+	i, err := strconv.Atoi(dtStr[:4])
+	if err != nil {
+		return yy, mm, dd
+	}
+	yy = i
+
+	i, err = strconv.Atoi(dtStr[4:6])
+	if err != nil {
+		return yy, mm, dd
+	}
+	mm = i
+
+	i, err = strconv.Atoi(dtStr[7:])
+	if err != nil {
+		return yy, mm, dd
+	}
+	dd = i
+	return yy, mm, dd
+}
+
+// a list of strings for each date in range to lookup
+func getDateRangeRegEx(dateFrom, dateTo string) []string {
+
+	regExpStr := []string{}
+	//'20160630'
+	yy, mm, dd := convertToDateParts(dateFrom)
+	dtFrom := time.Date(yy, time.Month(mm), dd, 0, 0, 0, 0, time.UTC)
+	if verbose {
+		log.Println("From:", dtFrom.String())
+	}
+
+	yy, mm, dd = convertToDateParts(dateTo)
+	dtTo := time.Date(yy, time.Month(mm), dd, 0, 0, 0, 0, time.UTC)
+	if verbose {
+		log.Println("To:", dtTo.String())
+	}
+
+	dt := dtFrom
+	for {
+		regExpStr = append(regExpStr, dt.Format("20060102"))
+		if verbose {
+			log.Printf("Appending for %s = %s\n", dt.String(), dt.Format("20060102"))
+		}
+
+		dt = dt.AddDate(0, 0, 1)
+		if dt.After(dtTo) {
+			break
+		}
+	}
+
+	return regExpStr
+}
+
+func printRangeString(dateRangeRegexStr []string) {
+	log.Println("Dates range:")
+	for _, str := range dateRangeRegexStr {
+		log.Println(str)
+	}
+}
+
+func main() {
+	startTime := time.Now()
+	countingDone := make(chan bool)
+
+	// This is our semaphore/pool
+	sem := make(chan bool, concurrency)
+
+	downloaded := 0
+
+	failedFilesChan = make(chan string)
+	downloadedReportChannel = make(chan bool)
+
+	msoList, MSOLookup = getMsoNamesList()
+
+	if verbose {
+		PrintParams()
+	}
+
+	dateRangeRegexStr := getDateRangeRegEx(dateFrom, dateTo)
+
+	failedFilesList := []string{}
+	var wg sync.WaitGroup
+
+	// Listening to failed reports
+	go func() {
+		for {
+			key, more := <-failedFilesChan
+			if more {
+				failedFilesList = append(failedFilesList, key)
+			} else {
+				return
+			}
+		}
+	}()
+
+	// listening to succeeded reports
+	go func() {
+		for {
+			_, more := <-downloadedReportChannel
+			if more {
+				downloaded++
+			} else {
+				countingDone <- true
+				return
+			}
+		}
+	}()
+
+	session := session.New(&aws.Config{
+		Region: aws.String(regionName),
+	})
+
+	svc := s3.New(session)
+
+	params := &s3.ListObjectsInput{
+		Bucket: aws.String(bucketName), // daap-viewership-reports
+		Prefix: aws.String("cdw-viewership-reports"),
+	}
+
+	// Get the list of all objects
+	resp, err := svc.ListObjects(params)
+	if err != nil {
+		log.Println("Failed to list objects: ", err)
+		os.Exit(-1)
+	}
+
+	log.Println("Number of objects: ", len(resp.Contents))
+	for _, key := range resp.Contents {
+		// iterate through the list to match the dates range/mso name
+		// using the constracted below lookup string
+
+		if verbose {
+			log.Println("Key: ", *key.Key)
+		}
+
+		for _, mso := range msoList {
+
+			for _, eachDate := range dateRangeRegexStr {
+				// cdw-data-reports/20160601/ Armstrong-Butler/hhid_count- Armstrong-Butler-20160601.csv
+				lookupKey := fmt.Sprintf("%s-%s.csv", mso.Name, eachDate)
+
+				if verbose {
+					log.Println("Lookup key: ", lookupKey)
+				}
+
+				if strings.Contains(*key.Key, lookupKey) {
+					// download the file (add to a queue of downloads)
+					// load the csv file, add the count to appropriate counter
+					// if we still have available goroutine in the pool (out of concurrency )
+					sem <- true
+					wg.Add(1)
+					go func(key string) {
+						defer func() { <-sem }()
+						processSingleDownload(key, &wg)
+					}(*key.Key)
+				}
+			}
+
+		}
+	}
+
+	// Now aggregate the counts and generate the aggregated report
+	// save the report csv?
+
+	// Reports
+	if verbose {
+		log.Println("All files sent to be downloaded. Waiting for completetion...")
+	}
+
+	for i := 0; i < cap(sem); i++ {
+		sem <- true
+	}
+
+	wg.Wait()
+	if verbose {
+		log.Println("All download jobs completed, closing failed/succeeded jobs channel")
+	}
+
+	close(failedFilesChan)
+	close(downloadedReportChannel)
+	<-countingDone
+
+	ReportFailedFiles(failedFilesList)
+
+	log.Println("Starting reading/aggregating the results")
+
+	fileList := []string{}
+	err = filepath.Walk("cdw-viewership-reports/", func(path string, f os.FileInfo, err error) error {
+		fileList = append(fileList, path)
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalln("Error walking the provided path: ", err)
+	}
+
+	var report ReportEntryList
+
+	for _, file := range fileList {
+		if isFileToPush(file) {
+			if verbose {
+				log.Println("Reading: ", file)
+			}
+			ss := ReadViewershipEntries(file)
+			report = append(report, ss...)
+		}
+	}
+
+	PrintFinalReport(report)
+	log.Printf("Processed %d MSO's, %d days, in %v\n", len(msoList), len(dateRangeRegexStr), time.Since(startTime))
+}
+
+func formatReportFilename(fileName string) string {
+	return fmt.Sprintf("%s-%s-%s.csv", fileName, dateFrom, dateTo)
+}
+
+func PrintFinalReport(report ReportEntryList) {
+	log.Println("Aggregated final:")
+
+	reportFileName := formatReportFilename("viewership-report")
+	out, err := os.Create(reportFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	defer out.Close()
+
+	writer := csv.NewWriter(out)
+
+	writer.WriteAll(report.Convert())
+
+	if err := writer.Error(); err != nil {
+		log.Fatalln("error writing csv:", err)
+	}
+
+	log.Println("Saved the report in file: ", reportFileName)
+
+	if verbose {
+		for _, entry := range report {
+			log.Println(entry)
+		}
+	}
+}
+
+// hh_id, ts, pg_id, pg_name, ch_num, ch_name, event, zipcode, country
+// 112961,2016-07-02 23:21:58,975540,"Oklahoma News Report",3,KETA,watch,79081,USA
+
+//aggregated reported entry
+type ReportEntry struct {
+	MsoId   string
+	MsoName string
+	hh_id   string
+	ts      string
+	pg_id   string
+	pg_name string
+	ch_num  string
+	ch_name string
+	event   string
+	zipcode string
+	country string
+}
+
+type ReportEntryList []ReportEntry
+
+// convert []ReportEntry into [][]string for csv file
+func (report ReportEntryList) Convert() [][]string {
+	header := []string{"Mso Id", "Mso Name", "hh_id", "ts", "pg_id", "pg_name", "ch_num", "ch_name", "event", "zipcode", "country"}
+	bodyAll := [][]string{}
+
+	bodyAll = append(bodyAll, header)
+
+	for _, entry := range report {
+		bodyAll = append(bodyAll,
+			[]string{
+				entry.MsoId,
+				entry.MsoName,
+				entry.hh_id,
+				entry.ts,
+				entry.pg_id,
+				entry.pg_name,
+				entry.ch_num,
+				entry.ch_name,
+				entry.event,
+				entry.zipcode,
+				entry.country,
+			})
+	}
+	return bodyAll
+}
+
+// Read hh count from a single file
+func ReadViewershipEntries(fileName string) []ReportEntry {
+	entries := []ReportEntry{}
+
+	entriesFile, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalf("Could not open Mso List file: %s, Error: %s\n", fileName, err)
+	}
+
+	r := csv.NewReader(entriesFile)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatalf("Could not read viewership file: %s, Error: %s\n", fileName, err)
+	}
+
+	msoId, msoName := GetMsoIdNameFrom(fileName)
+
+	for i, record := range records {
+		// Skipping the first line - header
+		if i > 0 {
+			// 	---			---			0		1		2		3			4		5			6		7			8
+			// "Mso Id", "Mso Name", "hh_id", "ts", "pg_id", "pg_name", "ch_num", "ch_name", "event", "zipcode", "country"
+			entries = append(entries, ReportEntry{msoId, msoName, record[0], record[1], record[2], record[3], record[4], record[5], record[6], record[7], record[8]})
+		}
+	}
+	return entries
+
+}
+
+func GetMsoIdNameFrom(fileName string) (string, string) {
+	return "", ""
+}
+
+func isFileToPush(fileName string) bool {
+	return filepath.Ext(fileName) == ".csv"
+}
+
+func ReportFailedFiles(failedFilesList []string) {
+	if len(failedFilesList) > 0 {
+		for _, key := range failedFilesList {
+			log.Println("Failed downloading: ", key)
+		}
+	} else {
+		log.Println("No failed downloads")
+	}
+}
+
+func processSingleDownload(key string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < maxAttempts; i++ {
+		log.Println("Downloading: ", key)
+		if downloadFile(key) {
+			if verbose {
+				log.Println("Successfully downloaded: ", key)
+			}
+			downloadedReportChannel <- true
+			return
+		}
+
+		if verbose {
+			log.Println("Failed, going to sleep for: ", key)
+		}
+		time.Sleep(time.Duration(10) * time.Second)
+
+	}
+	failedFilesChan <- key
+}
+
+func createPath(path string) error {
+	err := os.MkdirAll(filepath.Dir(path), os.ModePerm)
+	return err
+}
+
+func downloadFile(filename string) bool {
+
+	err := createPath(filename)
+	if err != nil {
+		log.Println("Could not create folder: ", filepath.Dir(filename))
+		return false
+	}
+
+	file, err := os.Create(filename)
+	if err != nil {
+		log.Println("Failed to create file: ", err)
+		return false
+	}
+
+	defer file.Close()
+
+	downloader := s3manager.NewDownloader(session.New(&aws.Config{Region: aws.String(regionName)}))
+
+	numBytes, err := downloader.Download(file,
+		&s3.GetObjectInput{
+			Bucket: aws.String(bucketName),
+			Key:    aws.String(filename),
+		})
+
+	if err != nil {
+		log.Printf("Failed to download file: %s, Error: %s ", filename, err)
+		return false
+	}
+
+	log.Println("Downloaded file ", file.Name(), numBytes, " bytes")
+	return true
+}
