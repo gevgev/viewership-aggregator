f759f478423f3ac73e5db3ed964dfbd3f76cbfc0
gevgev <gevgev@yahoo.com>
2016-07-31 18:13:45 -0700

    added check for dates to avoid unnecesary runs

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -16,4 +16,15 @@ to=${results[2]}
 
 echo $from, $to
 
+
+dateFrom=$(date -d "$from" +%s)
+dateTo=$(date -d "$to" +%s)
+
+if [ $dateFrom -ge $dateTo ]; then
+        echo "No need to run"
+        exit 0
+else
+    	echo "running"
+fi
+
 ./run.sh $from $to
